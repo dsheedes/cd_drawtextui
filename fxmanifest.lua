73f458725bdfef04cb29a3084b8943a2f6e07a84
Gvozden Despotovski <gvozdeniii@gmail.com>
2020-12-12 17:52:55 +0100

    push

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Codesign#2715'
+description 'Draw screen UI replacment for 3D text'
+version '1.0'
+
+ui_page {
+    'html/index.html',
+}
+
+files {
+	'html/index.html',
+	'html/js/script.js', 
+	'html/css/stylesheet.css',
+}
+
+client_scripts {
+	'client/main.lua'
+}
